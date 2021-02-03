40efdc1987fb6d35a58013bcf529ac5834df8daf
yogigachinmath <gachinmathyogi3@gmail.com>
2021-02-03 11:13:39 +0530

    Navbar changes -> add the logo, searchbar with ability to search and fetch the details from the api

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useCallback } from "react";
+import logo from "../../assets/Logo.svg";
+import "./Navbar.css";
+import { debounce } from "lodash";
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <nav className="nav-container">
+        <div className="nav-left-wrapper">
+          <a href="/" className="logo">
+            <img src={logo} alt="logo" height="66"></img>
+          </a>
+          <div className="searchbar">
+            <Search />
+          </div>
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Github</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+function Search() {
+  let [searchInput, setSearchInput] = useState();
+  let [suggestion, setSuggestion] = useState({});
+  let [toDisplaySuggestions, setToDisplaySuggestions ] = useState('none');
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+    if (event.target.value) fetchLyrics(event.target.value);
+  };
+
+  const fetchLyrics = useCallback(debounce((value) => {
+    fetch(`https://api.lyrics.ovh/suggest/${value}`)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setSuggestion(res);
+        });
+    }, 500),[]);
+  
+  const handleBlur = (event) => {
+    setToDisplaySuggestions('none');
+  }
+  const handleFocus = (event) => {
+    setToDisplaySuggestions('block');
+  }
+  return (
+    <div> 
+      <div>
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleChange}
+          placeholder="Type song title, artist or lyrics"
+          onBlur = {handleBlur}
+          onFocus = {handleFocus}
+        />
+      </div>
+      <div className="suggestion-box" style = {{display: toDisplaySuggestions }}>
+        {/* <a> see all</a> */}
+        {suggestion["data"] &&
+          suggestion["data"].map((res) => (
+            <div className="suggestion" key={res["id"]}>
+              <ul>
+                <li>
+                  <a href="/">
+                    <div className="suggestion-details">
+                      <h3>{res["title"]} </h3>
+                      <h5>{res["artist"]["name"]} </h5>
+                    </div>
+                  </a>
+                </li>
+                <hr></hr>
+              </ul>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
